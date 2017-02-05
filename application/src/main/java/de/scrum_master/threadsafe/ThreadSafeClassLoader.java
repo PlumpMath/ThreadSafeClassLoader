482aba1c09a57884e712dfa0e5c8928d788a3ea2
Alexander Kriegisch <Alexander@Kriegisch.name>
2017-02-05 13:56:11 +0100

    New class ThreadSafeClassLoader makes isolation easier to use

---
+++ application/src/main/java/de/scrum_master/threadsafe/ThreadSafeClassLoader.java
@@ -0,0 +1,48 @@
+package de.scrum_master.threadsafe;
+
+import org.xeustechnologies.jcl.JarClassLoader;
+import org.xeustechnologies.jcl.JclObjectFactory;
+
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreadSafeClassLoader extends JarClassLoader {
+  private static final JclObjectFactory OBJECT_FACTORY = JclObjectFactory.getInstance();
+
+  private final List<Class> classes = new ArrayList<>();
+
+  public static ThreadLocal<ThreadSafeClassLoader> create(Class... classes) {
+    return ThreadLocal.withInitial(
+      () -> new ThreadSafeClassLoader(classes)
+    );
+  }
+
+  private ThreadSafeClassLoader(Class... classes) {
+    super();
+    this.classes.addAll(Arrays.asList(classes));
+    for (Class clazz : classes) {
+      URL location = clazz.getProtectionDomain().getCodeSource().getLocation();
+      System.out.println(location);
+      add(location);
+    }
+  }
+
+  public Object newObject(Class clazz, Class[] argumentTypes, Object... arguments) {
+    if (!classes.contains(clazz))
+      throw new IllegalArgumentException("Class " + clazz + " is not protected by this thread-safe classloader");
+    return OBJECT_FACTORY.create(this, clazz.getName(), arguments, argumentTypes);
+  }
+
+  public Object invokeMethod(Object targetObject, String methodName, Class[] argumentTypes, Object... arguments) {
+    Class<?> clazz = targetObject.getClass();
+    try {
+      Method method = clazz.getMethod(methodName, argumentTypes);
+      return method.invoke(targetObject, arguments);
+    } catch (ReflectiveOperationException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
